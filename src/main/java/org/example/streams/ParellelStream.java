8c219ec2f6906c88540dd37b929b59c866993ecc
Ujjwal <Ujjwalrawat248@gmail.com>
2025-08-20 19:35:24 +0530

    [ADD:LazyEvaluationDemo]

---
+++ src/main/java/org/example/streams/ParellelStream.java
@@ -0,0 +1,31 @@
+package org.example.streams;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ParellelStream {
+    public static void main(String[] args) {
+        //using simple Stream
+//        long startTime=System.currentTimeMillis();
+//        Stream<Integer> integers=Stream.iterate(1,a->a+1).limit(20000);
+//        List<Long> factorials=integers.map(ParellelStream::factorial).toList();
+//        long endTime=System.currentTimeMillis();
+//        System.out.println("Total Time taken : "+(endTime-startTime)+" ms ");
+
+//        using Parellel Stream
+
+        long startTime=System.currentTimeMillis();
+        List<Integer> integers=Stream.iterate(1,a->a+1).limit(20000).toList();
+        List<Long> factorials=integers.parallelStream().map(ParellelStream::factorial).toList();
+        long endTime=System.currentTimeMillis();
+        System.out.println("Total Time taken : "+(endTime-startTime)+" ms ");
+    }
+
+    public static long factorial(int n){
+        long fact=1;
+        for(int i=1;i<=n;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+}
