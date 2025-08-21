b2094c2a90cc84ba287b2429e890fc543d8a9502
Ujjwal <Ujjwalrawat248@gmail.com>
2025-08-21 15:32:23 +0530

    [ADD :ParellelStream2Demo ]

--- src/main/java/org/example/streams/ParllelStream2Demo.java
+++ src/main/java/org/example/streams/ParllelStream2Demo.java
@@ -1,4 +1,17 @@
 package org.example.streams;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ParllelStream2Demo {
+    public static void main(String[] args) {
+        //here we will try to perform cumulative sum
+
+        List<Integer> ls= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        AtomicInteger sum=new AtomicInteger(0);
+        // run below one at a time to see the diffrence
+        //System.out.println(ls.parallelStream().map(sum::addAndGet).toList());
+        System.out.println(ls.stream().map(sum::addAndGet).toList());
+    }
 }
